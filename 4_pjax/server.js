7ed4dcb400dbde1dea2aea05c3a3cb96a0c9ad46
박진한 <parkjinhan@bagjinhan-ui-MacBookPro.local>
2023-06-17 09:41:13 +0900

    spa first commit

--- 4_pjax/server.js
+++ 4_pjax/server.js
@@ -1,20 +1,18 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = 5004;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/api/:page', (req, res) => {
+app.get("/api/:page", (req, res) => {
   const { page } = req.params;
   res.sendFile(path.join(__dirname, `/data/${page}.json`));
 });
 
-// 브라우저 새로고침을 위한 처리 (다른 route가 존재하는 경우 맨 아래에 위치해야 한다)
-// 브라우저 새로고침 시 서버는 index.html을 전달하고 클라이언트는 window.location.pathname를 참조해 다시 라우팅한다.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(port, () => {
